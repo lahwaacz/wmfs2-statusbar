8e0f29adb733567416d289de9524c73540996422
Jakub Klinkovský <kuba.klinkovsky@gmail.com>
2013-02-11 21:23:22 +0100

    Changed default theme

--- src/ConfigParser.cpp
+++ src/ConfigParser.cpp
@@ -14,7 +14,7 @@ string Config::get(string key) {
 
 // defaults - every plugin must specify default values here!
 void Config::setDefaults(void) {
-    dict["battery_format"]              = "^s[left;#aabbaa;Bat ]^s[left;#dddddd;%.1s %d%%]";
+    dict["battery_format"]              = "^s[left;#1793d0;Bat ]^s[left;#dddddd;%.1s %d%%]";
     dict["battery_path_full"]           = "/sys/class/power_supply/BAT0/energy_full";
     dict["battery_path_now"]            = "/sys/class/power_supply/BAT0/energy_now";
     dict["battery_path_state"]          = "/sys/class/power_supply/BAT0/status";
@@ -23,17 +23,17 @@ void Config::setDefaults(void) {
     dict["battery_critical_timeout"]    = "5";
     dict["battery_critical_action2"]    = "systemctl hibernate";
 
-    dict["cpu_format"]                  = "^s[left;#aabbaa;CPU ]^g[left;150;14;%d;100;#888888;#991111;cpu]";
+    dict["cpu_format"]                  = "^s[left;#1793d0;CPU ]^g[left;150;14;%d;100;#333333;#3f42b0;cpu]";
     dict["date_format"]                 = "^s[right;#dddddd;%s]";
-    dict["essid_format"]                = "^s[left;#aabbaa;ESSID]^s[left;#dddddd; %s]";
-    dict["ip_format"]                   = "^s[left;#aabbaa;IP]^s[left;#dddddd; %s]";
-    dict["network_format_wireless"]     = "^s[left;#aabbaa;Down ]^g[left;100;14;%d;300;#888888;#00af00;down] ^s[left;#888888; - ] ^s[left;#aabbaa;Up ]^g[left;100;14;%d;300;#888888;#af0000;up]";
-    dict["network_format_wired"]        = "^s[left;#aabbaa;Down ]^g[left;100;14;%d;5000;#888888;#00af00;down] ^s[left;#888888; - ] ^s[left;#aabbaa;Up ]^g[left;100;14;%d;5000;#888888;#af0000;up]";
+    dict["essid_format"]                = "^s[left;#1793d0;ESSID]^s[left;#dddddd; %s]";
+    dict["ip_format"]                   = "^s[left;#1793d0;IP]^s[left;#dddddd; %s]";
+    dict["network_format_wireless"]     = "^s[left;#1793d0;Down ]^g[left;100;14;%d;300;#333333;#1c9a34;down] ^s[left;#888888; - ] ^s[left;#1793d0;Up ]^g[left;100;14;%d;300;#333333;#bc1e1e;up]";
+    dict["network_format_wired"]        = "^s[left;#1793d0;Down ]^g[left;100;14;%d;5000;#888888;#00af00;down] ^s[left;#888888; - ] ^s[left;#1793d0;Up ]^g[left;100;14;%d;5000;#888888;#af0000;up]";
     dict["network_wired_interface"]     = "eth0";
     dict["network_wireless_interface"]  = "wlan0";
     dict["network_active_interface"]    = "";
-    dict["ram_format"]                  = "^s[left;#aabbaa;RAM ]^p[left;100;8;0;%ld;%ld;#888888;#771144]";
-    dict["volume_format"]               = "^s[left;#aabbaa;Vol ]^s[left;#dddddd;%d%%]";
+    dict["ram_format"]                  = "^s[left;#1793d0;RAM ]^p[left;100;8;0;%ld;%ld;#333333;#1d1e64]";
+    dict["volume_format"]               = "^s[left;#1793d0;Vol ]^s[left;#dddddd;%d%%]";
 }
 
 // set 'key=value' pair; ignored if 'key' doesn't already exist
